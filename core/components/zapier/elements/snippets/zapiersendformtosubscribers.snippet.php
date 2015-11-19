86dfe8569e3de2e5220a01a26d9844c6a7313769
sepiariver <info@sepiariver.com>
2015-11-18 20:31:21 -0800

    zapier send form snippet

--- core/components/zapier/elements/snippets/zapiersendformtosubscribers.snippet.php
+++ core/components/zapier/elements/snippets/zapiersendformtosubscribers.snippet.php
@@ -24,13 +24,14 @@
  * Place, Suite 330, Boston, MA 02111-1307 USA
  **/
 
-// We need some things
-if (!$hook) return false;
-$config = $formit->config;
-$values = $hook->getValues();
-$eventName = $getOption('zapierEventName', $config, 'new_form');
+// Pass control to next hook even if this one fails?
+$returnTrueOnFail = (bool) $modx->getOption('zapierReturnTrueOnFail', $formit->config, true);
 
-if (empty($values) || empty($eventName)) return;
+// We need some things
+if (!$hook) return $returnTrueOnFail;
+$values = $hook->getValues();
+$eventName = $getOption('zapierEventName', $formit->config, 'new_form');
+if (empty($values) || empty($eventName)) return $returnTrueOnFail;
 
 // Paths
 $zapierPath = $modx->getOption('zapier.core_path', null, $modx->getOption('core_path') . 'components/zapier/');
@@ -39,20 +40,52 @@ $zapierPath .= 'model/zapier/';
 // Get Classes
 if (file_exists($zapierPath . 'zapier.class.php')) $zapier = $modx->getService('zapier', 'Zapier', $zapierPath, $scriptProperties);
 if (!($zapier instanceof Zapier)) {
-    $modx->log(modX::LOG_LEVEL_ERROR, '[zapierAddSubscription] could not load the required Zapier class!');
-    return;
+    $modx->log(modX::LOG_LEVEL_ERROR, '[zapierSendFormToSubscribers] could not load the required Zapier class!');
+    return $returnTrueOnFail;
 }
 
 // Get Subscriptions
 $subscriptions = $modx->getCollection('ZapierSubscriptions', array('event' => $eventName));
 
 // Do stuff
+$successes = 0;
 foreach ($subscriptions as $sub) {
     
-    // If some some weird reason we don't have a target_url we gotta get out
-    if (!$sub->get('target_url')) continue;
+    // If some some weird reason we don't have a target_url, it's a bad record and should be removed
+    if (!$sub->get('target_url')) {
+        if ($sub->remove()) {
+            $noTargetError = '[zapierSendFormToSubscribers] discovered a ZapierSubscriptions object with no target_url, and removed it.';
+        } else {
+            $noTargetError = '[zapierSendFormToSubscribers] discovered a ZapierSubscriptions object ID: ' . $sub->get('id') . ' with no target_url, and failed to remove it.';
+        }
+        $modx->log(modX::LOG_LEVEL_ERROR, $noTargetError);
+        continue;
+    }
     
+    // cURL request to post form data
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HEADER, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0);
+    curl_setopt($ch, CURLOPT_URL, $sub->get('target_url'));
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 2500);
+    curl_setopt($ch, CURLOPT_TIMEOUT_MS, 7500);
     
+    $result = curl_exec($ch);
+    if (!empty($result)) $response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    
+    // According to Zapier: https://zapier.com/developer/documentation/v2/rest-hooks/
+    if ($response_code == '410') {
+        $modx->log(modX::LOG_LEVEL_ERROR, '[zapierSendFormToSubscribers] received a 410 from Zapier and removed the subscription ID: ' . $sub->get('id'));
+        $sub->remove();
+    } elseif ($response_code == '200') {
+        $successes++;
+    }
+
 }
 
-
+// If we got this far, we didn't fail
+if ($successes > 0) $returnTrueOnFail = true;
+return $returnTrueOnFail;
